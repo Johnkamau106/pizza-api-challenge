18ac3ab38a020b82b9e0b3a99f6871b484f6814f
Your Name <johnkamau106@gmail.com>
2025-06-14 16:23:51 +0300

    Refactor app.py for consistent indentation and import organization

--- server/app.py
+++ server/app.py
@@ -15,16 +15,16 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
-from .models import retraunt, pizza, restraunt_pizza
+    from .models import retraunt, pizza, restraunt_pizza
 
-from .controllers.restaurant_controller import restraunt_bp
-from .controllers.pizza_controller import pizza_bp
-from .controllers.restaurant_pizza_controller import restraunt_pizza_bp
-
-
-app.register_blueprint(restraunt_bp)
-app.register_blueprint(pizza_bp)
-app.register_blueprint(restraunt_pizza_bp)
+    from .controllers.restaurant_controller import restraunt_bp
+    from .controllers.pizza_controller import pizza_bp
+    from .controllers.restaurant_pizza_controller import restraunt_pizza_bp
 
 
-return app
+    app.register_blueprint(restraunt_bp)
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(restraunt_pizza_bp)
+
+
+    return app
